fb182808a74d53724d6a261122767c0a3c3e2427
Gautam Tiwari <193165968+Gautam-Sahil@users.noreply.github.com>
2025-01-18 15:00:07 +0530

    Add files via upload

---
+++ Watch-Project/script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const overlay = document.querySelector("[data-overlay]");
+const navbar = document.querySelector("[data-navbar]");
+const menuCloseBtn = document.querySelector("[data-nav-close-btn]");
+const menuOpenBtn = document.querySelector("[data-nav-open-btn]");
+
+const elemArr = [overlay, menuCloseBtn, menuOpenBtn];
+
+for (let i = 0; i < elemArr.length; i++) {
+  elemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+const navbarLinks = document.querySelectorAll("[data-navlink]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
